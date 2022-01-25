0973b98045aa4fd08ff512f81a1adad8aa559ba8
zainussi <98376572+zainussi@users.noreply.github.com>
2022-01-25 11:29:52 +0500

    Delete myapp directory

--- myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
-from .models import QA
-
-admin.site.register(QA)
-
-class QAAdmin(ImportExportModelAdmin):
-    list_display = ('question','answer')
--- myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import QA
-
-class QAData(forms.Form):
-	class meta:
-		model = QA
-		fields = '__all__'
